33810609b92da649a47ea2353cf3c406b0a25923
Prasannakumar018 <89631985+Prasannakumar018@users.noreply.github.com>
2021-09-08 22:34:08 +0530

    Factorial program

---
+++ Practice program 03.java
@@ -0,0 +1,15 @@
+class Cal
+{ 
+ 
+void fact1(int m){ 
+int fact1=1; 
+for(int i=1;i<=m;i++){ 
+ fact1=fact1*i; 
+} 
+System.out.println("factorial is "+fact2); 
+} 
+
+public static void main(String args[]){ 
+new Cal().fact1(5);
+} 
+}

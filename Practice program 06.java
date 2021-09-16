daef794ee35c0029d905977ca0212000ac3442c4
Prasannakumar018 <89631985+Prasannakumar018@users.noreply.github.com>
2021-09-16 22:26:02 +0530

    Create Practice program 06.java

---
+++ Practice program 06.java
@@ -0,0 +1,17 @@
+//purpose of default constructor
+class Stud1{ 
+int id1; 
+String name1; 
+
+void display()
+{
+  System.out.println(id1+" "+name1);
+  } 
+public static void main(String args[])
+{
+Stud1 s1=new Stud1(); 
+Stud1 s2=new Stud1(); 
+s1.display(); 
+s2.display(); 
+} 
+ }

02346e6384b5bba29b3bd5702f7592ea5cf9cb4b
Prasannakumar018 <89631985+Prasannakumar018@users.noreply.github.com>
2022-03-05 22:14:02 +0530

    Create Practice program 118.java

---
+++ Practice program 118.java
@@ -0,0 +1,6 @@
+public class HexToDecimalExample01{  
+public static void main(String args[]){  
+System.out.println(Integer.parseInt("a",16));  
+System.out.println(Integer.parseInt("f",16));  
+System.out.println(Integer.parseInt("121",16));  
+}}
